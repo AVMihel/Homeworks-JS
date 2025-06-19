6937bddd2448b103c210fe2fd7c48181ac60e5de
AVMihel <beglec2010@gmail.com>
2025-06-19 12:57:53 +0500

    Task complete

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,40 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant > 0) {
+    arr.push((-b + discriminant ** 0.5) / (2 * a));
+    arr.push((-b - discriminant ** 0.5) / (2 * a));
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (typeof percent !== 'number' ||
+      typeof contribution !== 'number' ||
+      typeof amount !== 'number' ||
+      typeof countMonths !== 'number') {
+    return false;
+  }
+
+  const monthlyPercent = percent / 100 / 12;
+
+  const loanBody = amount - contribution;
+  if (loanBody <= 0) {
+    return 0;
+  }
+
+  const monthlyPayment = loanBody * (
+    monthlyPercent +
+    (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1))
+  );
+
+  const totalAmount = monthlyPayment * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
