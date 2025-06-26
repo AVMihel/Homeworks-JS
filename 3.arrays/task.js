8ebdb8a753c09eb48c94136a28a5a85ae467cc02
AVMihel <beglec2010@gmail.com>
2025-06-26 13:55:47 +0500

    The tasks on the third topic have been completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length &&
+         arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const { count, sum } = users.reduce((acc, user) => {
+    if (user.gender === gender) {
+      acc.sum += user.age;
+      acc.count++;
+    }
+    return acc;
+  }, { count: 0, sum: 0 });
+
+  return count > 0 ? sum / count : 0;
 }
